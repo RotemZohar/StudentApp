c8d7eca02b4c605fff834679297f6ac8fcdb94b6
Rotem <rotem8888@gmail.com>
2022-05-13 21:40:49 +0300

    WIP

---
+++ StudentApp/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  StudentApp
+//
+//  Created by admin on 13/05/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var ContainerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StudentApp/Segue.swift
@@ -0,0 +1,16 @@
+//
+//  Segue.swift
+//  StudentApp
+//
+//  Created by admin on 13/05/2022.
+//
+
+import UIKit
+
+class Segue: UIStoryboardSegue {
+    
+    override func perform() {
+        <#code#>
+    }
+
+}
