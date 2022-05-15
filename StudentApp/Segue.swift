81bfda86387a4c2d4f69f67391f532f465015925
Rotem <rotem8888@gmail.com>
2022-05-15 19:21:25 +0300

    Make cancel work

--- StudentApp/Segue.swift
+++ StudentApp/Segue.swift
@@ -10,15 +10,28 @@ import UIKit
 class Segue: UIStoryboardSegue {
     
     override func perform() {
-        let source = self.source as! MainViewController
+        var view: MainViewController
+        let source = self.source
         let dest = self.destination
 
         
-        source.addChild(self.destination)
-        dest.view.frame = source.containerView.frame
+        if (source is MainViewController) {
+            view = source as! MainViewController
+        } else {
+            view = source.parent as! MainViewController
+        }
+        
+        if (view.children.count > 0 && view.containerView.subviews.count > 0) {
+            view.children[0].removeFromParent()
+            view.containerView.subviews[0].removeFromSuperview()
+        }
+        
+        view.addChild(self.destination)
+        dest.view.frame = view.containerView.frame
         dest.view.frame.origin.x = 0
         dest.view.frame.origin.y = 0
-        source.containerView.addSubview(dest.view)
+        view.containerView.addSubview(dest.view)
+        
     }
 
 }
